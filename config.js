dd15f28ca548ba554fe10a060c36572b44dc7eaf
Derrick Korir <derrickkip@Derricks-MacBook-Pro.local>
2019-03-04 14:32:56 +0300

    (feat): Implement CRUD for products - setup mongodb database - setup controllers - setup routes

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: 'mongodb://localhost:27017/products',
+  serverPort: 3000
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const config = require('./config.js');
+const mongoose = require('mongoose');
+require('./product.routes.js')(app);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.url, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log('Successfully connected to the database');
+}).catch(err => {
+  console.log('Could not connect to the database. Exiting now...', err);
+  process.exit();
+})
+
+app.get('/', (req, res) => {
+  res.json({"message": "Welcome to ZeptoBook Product app"});
+});
+
+app.listen(config.serverPort, () => {
+  console.log(`Server is listening on port ${config.serverPort}`);
+});
